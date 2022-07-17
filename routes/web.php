95707f28273a22f1f27222e3e4411ed1fa291d38
Matheus Gomes <math.s.gomes@gmail.com>
2022-07-16 23:32:55 -0300

    Remoção repetida middleware de autenticação

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,11 @@ Route::get('/', function() {
 });
 
 Route::middleware(['auth'])->group(function () {
-    Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy')->middleware('auth');
-    Route::put('users/{id}', [UserController::class, 'update'])->name('users.update')->middleware('auth');
-    Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit')->middleware('auth');
-    Route::get('/users', [UserController::class, 'index'])->name('users.index')->middleware('auth');
-    Route::get('/users/create', [UserController::class, 'create'])->name('users.create')->middleware('auth');
+    Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
+    Route::put('users/{id}', [UserController::class, 'update'])->name('users.update');
+    Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
     Route::post('/user', [UserController::class, 'store'])->name('users.store');
     Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
 
